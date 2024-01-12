5f38c4930410da00fd834a10cb7d367fb3a9c5a4
biswarup roy <69243820+biswarup55655@users.noreply.github.com>
2024-01-12 14:44:57 +0530

    Add files via upload
    GFG POTD JAN 12 : Reverse First K elements of Queue

---
+++ Jan12.java
@@ -0,0 +1,75 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.util.*;
+class Jan12 {
+    public static void main(String[] args) {
+
+        // Taking input using class Scanner
+        Scanner sc = new Scanner(System.in);
+
+        // Taking total number of testcases
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            // Taking count of total number of elements
+            int n = sc.nextInt();
+
+            // Taking count of total elements
+            // that need to be reversed
+            int k = sc.nextInt();
+
+            // Creating a Queue
+            Queue<Integer> q = new LinkedList<>();
+
+            // adding all the elements to the Queue
+            while (n-- > 0) {
+                q.add((int)sc.nextInt());
+            }
+
+            // Creating an object of class GfG
+            GfG g = new GfG();
+
+            // calling modifyQueue of class GfG
+            // and passing Queue and k as arguments
+            // and storing the reuslt in a new Queue
+            Queue<Integer> ans = g.modifyQueue(q, k);
+
+            // Printing all the elements from the
+            // new Queue and polling them out
+            while (!ans.isEmpty()) {
+                int a = ans.peek();
+                ans.poll();
+                System.out.print(a + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class GfG {
+    // Function to reverse first k elements of a queue.
+    public Queue<Integer> modifyQueue(Queue<Integer> q, int k) {
+        // add code here.
+        int n=q.size();
+        int rem = n - k;
+        Stack<Integer> s = new Stack<>();
+        while(k>0){
+            s.push(q.poll()); //q.front() -> q.pop()
+            k--;
+        }
+        while(!s.isEmpty()){
+            q.offer(s.pop()); //q.push()
+        }
+        while(rem>0){
+            q.offer(q.poll());
+            rem--;
+        }
+        return q;
+    }
+}
