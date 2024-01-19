3c1afcb831e1c0526673acb6e43e2e4d5408602d
biswarup roy <69243820+biswarup55655@users.noreply.github.com>
2024-01-20 05:06:57 +0530

    Add files via upload
    GFG POTD JAN20 : Distribute candies in a binary tree

--- Node.java
+++ Node.java
@@ -1,101 +1,126 @@
 //{ Driver Code Starts
 //Initial Template for Java
-
 import java.util.*;
-class Node
-    {
-        int data;
-        Node next;
-        Node(int d) {data = d; next = null; }
+import java.io.*;
+class Node {
+    int data;
+    Node left;
+    Node right;
+    Node(int data) {
+        this.data = data;
+        left = null;
+        right = null;
     }
-class insertion
-{
-    Node head;  
-    Node tail;
-	public void addToTheLast(Node node) 
-	{
-	  if (head == null) 
-	  {
-	   head = node;
-	   tail = node;
-	  } 
-	  else 
-	  {
-	   tail.next = node;
-	   tail = node;
-	  }
-	}
-      void printList(Node head)
-    {
-        Node temp = head;
-        while (temp != null)
-        {
-           System.out.print(temp.data+" ");
-           temp = temp.next;
-        }  
-        System.out.println();
-    }
-	/* Drier program to test above functions */
-	public static void main(String args[])
-    {
-         Scanner sc = new Scanner(System.in);
-		 int t=sc.nextInt();
-		 while(t>0)
-         {
-			int n = sc.nextInt();
-			insertion llist = new insertion(); 
-			int a1=sc.nextInt();
-			Node head= new Node(a1);
-            llist.addToTheLast(head);
-            for (int i = 1; i < n; i++) 
-			{
-				int a = sc.nextInt(); 
-				llist.addToTheLast(new Node(a));
-			}
-			
-        Solution ob = new Solution();
-		head = ob.insertionSort(llist.head);
-		llist.printList(head);
-		
-        t--;		
+} class GfG {
+    public static Node buildTree(String str) {
+
+        if (str.length() == 0 || str.charAt(0) == 'N') {
+            return null;
         }
-    }}
+
+        String ip[] = str.split(" ");
+        // Create the root of the tree
+        Node root = new Node(Integer.parseInt(ip[0]));
+        // Push the root to the queue
+
+        Queue<Node> queue = new LinkedList<>();
+
+        queue.add(root);
+        // Starting from the second element
+
+        int i = 1;
+        while (queue.size() > 0 && i < ip.length) {
+
+            // Get and remove the front of the queue
+            Node currNode = queue.peek();
+            queue.remove();
+
+            // Get the current node's value from the string
+            String currVal = ip[i];
+
+            // If the left child is not null
+            if (!currVal.equals("N")) {
+
+                // Create the left child for the current node
+                currNode.left = new Node(Integer.parseInt(currVal));
+                // Push it to the queue
+                queue.add(currNode.left);
+            }
+
+            // For the right child
+            i++;
+            if (i >= ip.length) break;
+
+            currVal = ip[i];
+
+            // If the right child is not null
+            if (!currVal.equals("N")) {
+
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+
+                // Push it to the queue
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+
+        return root;
+    }
+   
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+
+        int t = Integer.parseInt(br.readLine().trim());
+
+        while (t-- > 0) {
+            
+            String s = br.readLine();
+            Node root = buildTree(s);
+         
+            Solution ob=new Solution();
+            System.out.println( ob.distributeCandy(root));
+            
+        }
+    }
+}
 // } Driver Code Ends
 
 
 //User function Template for Java
 
-/*class Node
-    {
-        int data;
-        Node next;
-        Node(int d) {data = d; next = null; }
+/*
+class Node {
+    int data;
+    Node left;
+    Node right;
+    Node(int data) {
+        this.data = data;
+        left = null;
+        right = null;
     }
-    */
+}*/
+
+
 class Solution
 {
-    public static Node insertionSort(Node head_ref)
+    static int step;
+    public static int distributeCandy(Node root)
     {
         //code here
-        Node dummy = new Node(0);
-        dummy.next = head_ref;
-        Node curr = head_ref.next;
-        Node lastSorted = head_ref;
-        while(curr != null){
-            if(curr.data < lastSorted.data){
-                Node prev = dummy;
-                while(prev.next.data < curr.data){
-                    prev = prev.next;
-                }
-                lastSorted.next = curr.next;
-                curr.next = prev.next;
-                prev.next = curr;
-                curr = lastSorted.next;
-            }else{
-                lastSorted =  curr;
-                curr = curr.next;
-            }
+        step=0;
+        helper(root);
+        return step;
+    }
+    static int helper(Node root){
+        if(root==null){
+            return 0;
         }
-        return dummy.next;
+        int l = helper(root.left);
+        int r = helper(root.right);
+        step+= Math.abs(l)+Math.abs(r);
+        return (l+r+root.data-1);
     }
 }
